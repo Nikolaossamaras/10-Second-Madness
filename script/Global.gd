47e9b0e3b0ec70216ecef8371fdf99ee4e1346c0
Nikolaos Samaras <nikossamaras62@gmail.com>
2026-08-14 02:46:44 +0300

    deathscreen

--- script/Global.gd
+++ script/Global.gd
@@ -1,11 +1,15 @@
 extends Node
 
-var lives = 5
+var lives = 1
 var minigames_done = 0
 var ships_killed =0
 var ship_needed = 10
 
 func _process(delta: float) -> void:
+	if lives == 0:
+		get_tree().change_scene_to_file("res://scene/deathscreen.tscn")
+		lives =5
+		minigames_done = 0
 	if ships_killed == ship_needed:
 		get_tree().change_scene_to_file("res://scene/real_winscreen.tscn")
 		ships_killed = 0
